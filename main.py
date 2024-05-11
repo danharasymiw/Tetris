26c5a3675be3173b3fa38df924867777d04bbd36
Dan <danharasymiw@gmail.com>
2024-05-11 15:01:10 -0400

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,159 @@
+
+# Tetris
+def new_board():
+    return [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [-1, -1, -1, -1, -1]
+    ]
+
+board = new_board()
+
+piece_x = 2
+piece_y = -2
+
+piece_1 = [
+    [0, 1],
+    [0, 1]
+]
+
+piece_2 = [
+    [0, 0],
+    [1, 1]
+]
+
+pieces = [piece_1, piece_2]
+
+board_indexes = [0, 1, 2, 3, 4]
+reversed_board_indexes = [4, 3, 2, 1, 0]
+piece_indexes = [0, 1]
+
+curr_piece = pieces[randint(0, 1)]
+
+piece_num = 1
+
+def draw_piece():
+    global curr_piece, piece_x, piece_y
+
+    for y in piece_indexes:
+        for x in piece_indexes:
+            if curr_piece[y][x]:
+                led.plot(piece_x + x, piece_y + y)
+
+def draw_board():
+    for y in board_indexes:
+        for x in board_indexes:
+            if board[y][x]:
+                led.plot(x, y)
+
+def redraw():
+    basic.clear_screen()
+    draw_board()
+    draw_piece()
+
+is_playing = True
+
+def add_piece_to_board():
+    global piece_x, piece_y, curr_piece, board, piece_num
+    piece_y = piece_y - 1
+    for y in piece_indexes:
+       for x in piece_indexes:
+            if curr_piece[y][x]:
+                if y + piece_y <= 0:
+                    return False
+                board[y + piece_y][x + piece_x] = piece_num
+    piece_y = -2
+    piece_x = 2
+    curr_piece = pieces[randint(0, 1)]
+
+    return True
+
+def piece_collides(piece_x, piece_y):
+    global curr_piece, board
+    for y in piece_indexes:
+        for x in piece_indexes:
+            if curr_piece[y][x] and \
+                0 <= piece_y + y < len(board_indexes) + 2 and \
+                0 <= piece_x + x < len(board_indexes) + 2 and \
+                board[piece_y + y][piece_x + x]:
+                    return True
+    return False
+
+
+def move_piece(direction):
+    global piece_x, piece_y
+    if piece_on_board(piece_x + direction, piece_y) and \
+        not piece_collides(piece_x + direction, piece_y):
+       piece_x = piece_x + direction
+  
+    redraw()
+
+def on_button_pressed_a():
+    move_piece(-1)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    move_piece(1)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def rows_completed():
+    rows = []
+    for y in board_indexes:
+        count = 0
+        for x in board_indexes:
+            if board[y][x]:
+                count = count + 1
+            if count >= 5:
+                board[y] = [0, 0, 0, 0, 0]
+                rows.append(y)
+    return rows
+
+def shift_down(rows):
+    global board
+    
+    for row_num in rows:
+        for y in range(row_num, 0, -1):
+            board[int(y)] = board[int(y)-1]
+        redraw()
+        basic.pause(500)
+    board[0] = [0, 0, 0, 0, 0]
+    redraw()   
+
+def piece_on_board(x, y):
+    global curr_piece
+    for piece_y in piece_indexes:
+        for piece_x in piece_indexes:
+            if not on_board(piece_x + x, piece_y + y):
+                return False
+    return True
+
+def on_board(x, y):
+    return 0 <= x < 5 and 0 <= y < 5
+
+def on_forever():
+    global piece_x, piece_y, pieces, curr_piece, piece_num, is_playing
+    while True:
+        while True:
+            led.plot(0, 0)
+            basic.pause(500)
+            basic.clear_screen()
+            
+            piece_y = piece_y + 1
+
+            if piece_collides(piece_x, piece_y):
+                if not add_piece_to_board():
+                    break
+
+                piece_num = piece_num + 1
+
+                shift_down(rows_completed())
+            
+            redraw()
+
+        basic.show_number(piece_num)
+
+
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,167 @@
+//  Tetris
+function new_board(): number[][] {
+    return [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [-1, -1, -1, -1, -1]]
+}
+
+let board = new_board()
+let piece_x = 2
+let piece_y = -2
+let piece_1 = [[0, 1], [0, 1]]
+let piece_2 = [[0, 0], [1, 1]]
+let pieces = [piece_1, piece_2]
+let board_indexes = [0, 1, 2, 3, 4]
+let reversed_board_indexes = [4, 3, 2, 1, 0]
+let piece_indexes = [0, 1]
+let curr_piece = pieces[randint(0, 1)]
+let piece_num = 1
+function draw_piece() {
+    
+    for (let y of piece_indexes) {
+        for (let x of piece_indexes) {
+            if (curr_piece[y][x]) {
+                led.plot(piece_x + x, piece_y + y)
+            }
+            
+        }
+    }
+}
+
+function draw_board() {
+    for (let y of board_indexes) {
+        for (let x of board_indexes) {
+            if (board[y][x]) {
+                led.plot(x, y)
+            }
+            
+        }
+    }
+}
+
+function redraw() {
+    basic.clearScreen()
+    draw_board()
+    draw_piece()
+}
+
+let is_playing = true
+function add_piece_to_board(): boolean {
+    
+    piece_y = piece_y - 1
+    for (let y of piece_indexes) {
+        for (let x of piece_indexes) {
+            if (curr_piece[y][x]) {
+                if (y + piece_y <= 0) {
+                    return false
+                }
+                
+                board[y + piece_y][x + piece_x] = piece_num
+            }
+            
+        }
+    }
+    piece_y = -2
+    piece_x = 2
+    curr_piece = pieces[randint(0, 1)]
+    return true
+}
+
+function piece_collides(piece_x: number, piece_y: number): boolean {
+    
+    for (let y of piece_indexes) {
+        for (let x of piece_indexes) {
+            if (curr_piece[y][x] && (0 <= piece_y + y && piece_y + y < board_indexes.length + 2) && (0 <= piece_x + x && piece_x + x < board_indexes.length + 2) && board[piece_y + y][piece_x + x]) {
+                return true
+            }
+            
+        }
+    }
+    return false
+}
+
+function move_piece(direction: number) {
+    
+    if (piece_on_board(piece_x + direction, piece_y) && !piece_collides(piece_x + direction, piece_y)) {
+        piece_x = piece_x + direction
+    }
+    
+    redraw()
+}
+
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    move_piece(-1)
+})
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    move_piece(1)
+})
+function rows_completed(): any[] {
+    let count: number;
+    let rows = []
+    for (let y of board_indexes) {
+        count = 0
+        for (let x of board_indexes) {
+            if (board[y][x]) {
+                count = count + 1
+            }
+            
+            if (count >= 5) {
+                board[y] = [0, 0, 0, 0, 0]
+                rows.push(y)
+            }
+            
+        }
+    }
+    return rows
+}
+
+function shift_down(rows: any[]) {
+    
+    for (let row_num of rows) {
+        for (let y = row_num; y > 0; y += -1) {
+            board[Math.trunc(y)] = board[Math.trunc(y) - 1]
+        }
+        redraw()
+        basic.pause(500)
+    }
+    board[0] = [0, 0, 0, 0, 0]
+    redraw()
+}
+
+function piece_on_board(x: any, y: number): boolean {
+    
+    for (let piece_y of piece_indexes) {
+        for (let piece_x of piece_indexes) {
+            if (!on_board(piece_x + x, piece_y + y)) {
+                return false
+            }
+            
+        }
+    }
+    return true
+}
+
+function on_board(x: any, y: any) {
+    return 0 <= x && x < 5 && (0 <= y && y < 5)
+}
+
+basic.forever(function on_forever() {
+    
+    while (true) {
+        while (true) {
+            led.plot(0, 0)
+            basic.pause(500)
+            basic.clearScreen()
+            piece_y = piece_y + 1
+            if (piece_collides(piece_x, piece_y)) {
+                if (!add_piece_to_board()) {
+                    break
+                }
+                
+                piece_num = piece_num + 1
+                shift_down(rows_completed())
+            }
+            
+            redraw()
+        }
+        basic.showNumber(piece_num)
+    }
+})
